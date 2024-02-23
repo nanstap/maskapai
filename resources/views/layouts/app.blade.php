20134a4ebf69dc384d6e4df343b70cbf07c1ceb0
adipramandev <herryprasetyo.dev@gmail.com>
2024-02-23 20:16:55 +0700

    menu navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,11 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
 
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('penerbangan.index') }}">Penerbangan</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
